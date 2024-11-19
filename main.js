c120b4fcd161896da81960509b61b65e20d11278
cyberSkunk <segun8@outlook.com>
2024-11-19 22:13:01 +0000

    fist commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const slideShowImages = document.querySelectorAll(".slides .slideshow-img")
+
+let currentImageCounter = 0;
+
+slideShowImages[currentImageCounter].style.display = "block"
+
+let nextImageDelay = 1500;
+
+
+setInterval(nextImage, nextImageDelay)
+
+function nextImage(){
+  slideShowImages[currentImageCounter].style.display = "none"
+  currentImageCounter = (currentImageCounter + 1) % slideShowImages.length
+  slideShowImages[currentImageCounter].style.display = "block"
+}
+
